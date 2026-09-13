5b7fe24bdb7399ba0d0c94f9b690783cbe1774bf
Leo Baiano <ljunior2005@gmail.com>
2026-09-13 08:02:08 -0300

    feat: cria o contexto accounts com função para criação de usuários no banco

---
+++ lib/event_flow/accounts/accounts.ex
@@ -0,0 +1,28 @@
+defmodule EventFlow.Accounts do
+  @moduledoc """
+  O contexto Accounts é a fronteira (boundary) do domínio de gerenciamento de usuários.
+  """
+
+  import Ecto.Query, warn: false
+  alias EventFlow.Repo
+  alias EventFlow.Accounts.User
+
+  @doc """
+  Cria um usuário no banco de dados a partir de um mapa de atributos.
+
+  ## Exemplos
+
+      iex> create_user(%{email: "valid@email.com", password: "123456"})
+      {:ok, %User{}}
+
+      iex> create_user(%{email: "bad", password: "123"})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  @spec create_user(map()) :: {:ok, User.t()} | {:error, Ecto.Changeset.t()}
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+end
